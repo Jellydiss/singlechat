cc882e23186ffa2d803b6a4d622ae884180ff9cc
kyunooh <chm073@gmail.com>
2014-06-17 15:34:45 +0900

    Maven base setting

---
+++ singlechat/src/main/java/Jellydiss/singlechat/App.java
@@ -0,0 +1,13 @@
+package Jellydiss.singlechat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
