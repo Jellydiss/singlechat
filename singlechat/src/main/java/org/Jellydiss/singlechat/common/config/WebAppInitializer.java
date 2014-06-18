0f1167c5e5f743d20f9ff602c13c5751b8b73d82
kyunooh <chm073@gmail.com>
2014-06-18 12:02:21 +0900

    Spring 4.0 setting

---
+++ singlechat/src/main/java/org/Jellydiss/singlechat/common/config/WebAppInitializer.java
@@ -0,0 +1,20 @@
+package org.Jellydiss.singlechat.common.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.servlet.DispatcherServlet;
+
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext container) {
+        ServletRegistration.Dynamic registration =
+                container.addServlet("dispatcher", new DispatcherServlet());
+              registration.setLoadOnStartup(1);
+              registration.addMapping("*.do");
+    }
+
+ }
