2ab842d40f8041a6868db06f224292b0db938b8f
kyunooh <chm073@gmail.com>
2014-06-21 17:07:12 +0900

    non-xml HelloWorld complete

---
+++ singlechat/src/main/java/org/jellydiss/singlechat/common/config/MvcConfiguration.java
@@ -0,0 +1,10 @@
+package org.jellydiss.singlechat.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+class MvcConfiguration extends WebMvcConfigurerAdapter{
+}
