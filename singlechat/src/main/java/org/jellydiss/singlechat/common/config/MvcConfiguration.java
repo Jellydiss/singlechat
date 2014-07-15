e65c58ddbbaf43e7505b02276af4516d36880028
kyunooh <chm073@gmail.com>
2014-07-15 15:07:14 +0900

    Update class for interceptor registry

--- singlechat/src/main/java/org/jellydiss/singlechat/common/config/MvcConfiguration.java
+++ singlechat/src/main/java/org/jellydiss/singlechat/common/config/MvcConfiguration.java
@@ -1,9 +1,11 @@
 package org.jellydiss.singlechat.common.config;
 
+import org.jellydiss.singlechat.common.config.interceptor.ChatInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -30,4 +32,8 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter {
 		  registry.addResourceHandler("/js/**")
 		    .addResourceLocations("/js/");
 		}
+	@Override
+	public void addInterceptors(InterceptorRegistry registry){
+		registry.addInterceptor(new ChatInterceptor());
+	}
 }
